fd406122a218deb84493decef435b2e4cb8932cf
lilusheng687 <lilusheng687@163.com>
2019-12-02 17:12:26 +0800

    切割头部和尾部子视图

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer>
+    <img class="brand-icon" src="/img/1575277746.jpg"/>
+    <a href="https://www.baidu.com" target="_blank" >
+        刻意练习，每日精进
+    </a>
+    <div class="float-right">
+        <a href="/about">关于</a>
+    </div>
+</footer>
