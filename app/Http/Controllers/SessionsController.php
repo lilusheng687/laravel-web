f9c6eb4f8d7d3cd2feee2d625477f8a33f8ebef9
lilusheng687 <lilusheng687@163.com>
2019-12-03 16:27:59 +0800

    用户退户登录

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -4,25 +4,35 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Auth;
+
 class SessionsController extends Controller
 {
-    public function create(){
+    public function create()
+    {
         return view('sessions.create');
     }
-    public function store(Request $request){
-        $credentials=$this->validate($request,[
-            'email'=>'required|email|max:255',
-            'password'=>'required'
+
+    public function store(Request $request)
+    {
+        $credentials = $this->validate($request, [
+            'email' => 'required|email|max:255',
+            'password' => 'required'
         ]);
-        if(Auth::attempt($credentials)){
+        if (Auth::attempt($credentials)) {
             //登录成功后的相关操作
-            session()->flash('success','欢迎回来！');
-            return redirect()->route('users.show',[Auth::user()]);
-        }else{
+            session()->flash('success', '欢迎回来！');
+            return redirect()->route('users.show', [Auth::user()]);
+        } else {
             //登录失败后的相关操作
             return redirect()
                 ->back()
                 ->withErrors('抱歉，您的邮箱和密码不匹配！');
         }
+    }
+
+    public function destroy(){
+        Auth::logout();
+        session()->flash("success",'您已成功退出');
+        return redirect('login');
     }
 }
